96dd9ba1eca4266d8e44e955bd2b87239e691c3a
fareezulhafiz <fareezulhafiz15@gmail.com>
2024-10-20 13:43:27 +0700

    Golang Part 1 Update
    1) Variables & Constants in Go
    2) Formatted Output - printf
    3) Data Types in Go
    4) Getting User Input
    5) Pointer
    6) Book Ticket Logic
    7) Arrays & Slices
    8) Loops in Go
    9) Conditionals (if / else) and Boolean Data Type
    10) Validate User Input
    11) Switch Statement

--- main.go
+++ main.go
@@ -1,16 +1,92 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main(){
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+	bookings := []string{}
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
+		// ask user for their name
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
+
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
+
+		fmt.Println("Enter your email: ")
+		fmt.Scan(&email)
+
+		fmt.Println("Enter number of tickets: ")
+		fmt.Scan(&userTickets)
+
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email,"@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+
+
+		if isValidName && isValidEmail && isValidTicketNumber {
+
+			remainingTickets = remainingTickets - userTickets
+			bookings = append(bookings, firstName + " " + lastName)
+
+			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+			fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				firstNames = append(firstNames, names[0])
+			}
+			fmt.Printf("The first names of booings are: %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				//end program
+				fmt.Println("Our conference is booked out. Come back next year")
+				break
+			}		
+
+		} else {
+
+			if !isValidName{
+				fmt.Println("First name or last name you entered is too short")
+			}
+			if !isValidEmail{
+				fmt.Println("Email address you entered does not contain @ sign")
+			}
+			if !isValidTicketNumber{
+				fmt.Println("Number of tickets you entered is invalid")
+			}
+			
+		}
+	}
+
+	city := "London"
+
+	switch city {
+		case "New York":
+			// execute code for booking New York conference tickets
+		case "Singapore", "Hong Kong":
+			// execute code for booking Singapore and Hong Kong conference tickets
+		case "London", "Berlin":
+			// execute code for booking London and Berlin conference tickets
+		case "Mexico City":
+			// execute code for booking Mexico City conference tickets
+		default:
+			fmt.Print("No valid city selected")
+	}
 }
